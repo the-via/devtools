d0921f3450b13f249193b0b7766af7c9ce0bd52d
Olivia Briggs <ofbriggs@gmail.com>
2021-03-19 13:45:54 -0400

    Attempting to get content-scripts running

--- js/devtools.js
+++ js/devtools.js
@@ -1,1 +1,12 @@
-chrome.devtools.panels.create("VIA", null, "/html/panel.html", null);
+chrome.devtools.panels.create("VIA", null, "/html/panel.html", (panel)=>{
+  panel.onShown.addListener(function(window) {
+chrome.devtools.inspectedWindow.eval("console.log('shown');");
+}), panel.onHidden.addListener(function() {
+chrome.devtools.inspectedWindow.eval("console.log('hidden');");
+});
+});
+console.error(Object.keys(chrome.tabs));
+let color = 'blue';
+setInterval(()=>{
+chrome.devtools.inspectedWindow.eval("alert(3);");
+}, 5000);
